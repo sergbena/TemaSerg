106afac60a5dc8ef7946661b371eade21879b4cb
samsung <sbenavidesvazquez@danielcastelao.org>
2016-01-29 14:08:03 +0100

    openerp

--- Mitema/__openerp__.py
+++ Mitema/__openerp__.py
@@ -1,1 +1,13 @@
 
+{
+'name': 'Bena tema',
+'description' : 'La descripcion de mi tema',
+'version':'1.0',
+'author': 'Sergio Benavides',
+
+'data':[
+],
+
+'category':'theme/creative',
+'depends':['website','website_less']
+}
